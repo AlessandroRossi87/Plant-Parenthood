4f9f5e007e2ecd4d1ffa4997e0b890687e5fed1c
Alessandro Rossi <rossisverige@yahoo.se>
2023-11-29 13:18:43 +0000

    Created form inside signupform

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
-import axios from "axios";
+import axios from 'axios';
 
-axios.defaults.baseURL = 'https://plant-parenthood-pp5-ac00fe42de7c.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.baseURL = 'https://plant-parenthood-pp5-ac00fe42de7c.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
