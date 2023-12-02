00a3e5f0c8d857ac2109086b7f8d0662cff1f63b
Alessandro Rossi <rossisverige@yahoo.se>
2023-12-02 13:11:34 +0000

    Implemented search bar

--- src/pages/plants/PlantsPage.js
+++ src/pages/plants/PlantsPage.js
@@ -14,16 +14,20 @@ import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+// import { fetchMoreData } from "../../utils/utils";
 
 function PlantsPage({ message, filter = "" }) {
   const [plants, setPlants] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
+  const [query, setQuery] = useState("")
+
   useEffect(() => {
     const fetchPlants = async () => {
       try {
-        const { data } = await axiosReq.get(`/plants/?${filter}`);
+        const { data } = await axiosReq.get(`/plants/?${filter}search=${query}`);
         setPlants(data);
         setHasLoaded(true);
       } catch (err) {
@@ -32,13 +36,33 @@ function PlantsPage({ message, filter = "" }) {
     };
 
     setHasLoaded(false);
-    fetchPlants();
-  }, [filter, pathname]);
+    const timer = setTimeout(() => {
+      fetchPlants();
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, query, pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search plants"
+          />
+        </Form>
+
         {hasLoaded ? (
           <>
             {plants.results.length ? (
