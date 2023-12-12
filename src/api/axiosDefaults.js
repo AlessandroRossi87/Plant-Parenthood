60072d16826ed7cf7ebb3311a1c6445c0a34b09a
Alessandro Rossi <rossisverige@yahoo.se>
2023-12-12 11:35:23 +0000

    Changed base path for API route

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://plant-parenthood-pp5-ac00fe42de7c.herokuapp.com/';
+axios.defaults.baseURL = '/api';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
